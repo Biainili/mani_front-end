0114283254b3612d2cf3f80c95e9cd5b5ad9a24d
Biainili <barkhudaryan005@gmail.com>
2024-06-26 11:56:28 +0400

    Commit_4

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import Button from '../Button/Button'
-import { useTelegram } from '../../hooks/useTelegram'
+import React from 'react';
+import Button from '../Button/Button';
+import { useTelegram } from '../../hooks/useTelegram';
 
 export default function Header() {
-const {user, onClose, onToggleButton} = useTelegram();
- 
+    const { user, onClose, onToggleButton } = useTelegram();
+
+    console.log('User:', user); // Добавлено для отладки
+
     return (
         <div className={'header'}>
             <Button onClick={onClose}>Close</Button>
-            <h1>{user?.Username}</h1>
+            <h1>{user?.username}</h1> {/* Исправлено имя свойства */}
             <span className={'username'}>
-                {user?.Username}
+                {user?.username} {/* Исправлено имя свойства */}
             </span>
         </div>
-    )
+    );
 }
