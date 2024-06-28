cc7da05fb3f6d6376ed31cbffc922c36d14c92fe
Biainili <barkhudaryan005@gmail.com>
2024-06-28 12:19:54 +0400

    Adds14

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Button from '../Button/Button';
 import { useTelegram } from '../../hooks/useTelegram';
+import { useNavigate } from 'react-router-dom';
 import "./Header.css";
 
 export default function Header() {
     const { user, onClose, onToggleButton } = useTelegram();
+    const navigate = useNavigate();
+
+    
+    const handleOrderClick = () => {
+        navigate('/order');
+    };
 
 
     return (
@@ -16,7 +23,7 @@ export default function Header() {
                 <Button onClick={onClose}>Закрыть</Button>
             </div>
             <div className={'header'}>
-                <Button onClick={onClose}>Создать Заказ</Button>
+                <Button onClick={handleOrderClick}>Создать Заказ</Button>
                 <Button onClick={onClose}>Наши Товары</Button>
                 <Button onClick={onClose}>Связ с Админом</Button>
             </div>
