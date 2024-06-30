b2ee86b369c08ad30c803aea4e91cf2b2f3b23a4
ArmWinners <127731412+ACAourteam@users.noreply.github.com>
2024-06-30 13:20:54 +0400

    Add Name

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,34 +1,37 @@
-import React from 'react';
-import Button from '../Button/Button';
-import { useTelegram } from '../../hooks/useTelegram';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import Button from "../Button/Button";
+import { useTelegram } from "../../hooks/useTelegram";
+import { useNavigate } from "react-router-dom";
 import "./Header.css";
 
 export default function Header() {
-    const { user, onClose, onToggleButton } = useTelegram();
-    const navigate = useNavigate();
+  const { user, onClose, onToggleButton } = useTelegram();
+  const navigate = useNavigate();
 
+  const handleOrderClick = () => {
+    navigate("/order");
+  };
+  const handleMainClick = () => {
+    navigate("/");
+  };
 
-    const handleOrderClick = () => {
-        navigate('/order');
-    };
-   const handleMainClick = () => {
-        navigate('/');
-    };
-
-    return (
-        <div className={"nav_bar"}>
-            <div className='user_header'>
-                <span className={'username'}>
-                    {user?.username}
-                </span>
-                <Button onClick={onClose}>Закрыть</Button>
-            </div>
-            <div className={'header'}>
-                <Button onClick={handleOrderClick}>Создать Заказ</Button>
-                <Button onClick={handleMainClick}>Наши Товары</Button>
-                <Button onClick={onClose}>Связ с Админом</Button>
-            </div>
+  return (
+    <div className={"nav_bar"}>
+      <div className="user_header">
+        <div className={"name_container"}>
+          <span className={"username"}>
+            {user?.first_name} {user?.last_name}
+          </span>
+          <span className={"username"}>({user?.username})</span>
         </div>
-    );
+
+        <Button onClick={onClose}>Закрыть</Button>
+      </div>
+      <div className={"header"}>
+        <Button onClick={handleOrderClick}>Создать Заказ</Button>
+        <Button onClick={handleMainClick}>Наши Товары</Button>
+        <Button onClick={onClose}>Связ с Админом</Button>
+      </div>
+    </div>
+  );
 }
